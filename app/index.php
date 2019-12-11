d82966fb72d6df99b1ae1f9e900e693a3c4723b0
Pavle <pavlito1988@gmail.com>
2019-12-11 12:05:06 +0100

    init

---
+++ app/index.php
@@ -0,0 +1,250 @@
+<?php require '_global.php'; ?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+		
+		<title>Disclaimer Generator: Generate Your Disclaimer</title>
+		<meta name="description" content="World's Easiest Disclaimer Generator: Generate your Disclaimer in just 10 seconds" />
+		
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/images/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/images/apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/images/apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/images/apple-touch-icon-144x144.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/images/apple-touch-icon-60x60.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/images/apple-touch-icon-120x120.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="/assets/images/apple-touch-icon-76x76.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/assets/images/apple-touch-icon-152x152.png" />
+		<link rel="icon" type="image/png" href="/assets/images/favicon-196x196.png" sizes="196x196" />
+		<link rel="icon" type="image/png" href="/assets/images/favicon-96x96.png" sizes="96x96" />
+		<link rel="icon" type="image/png" href="/assets/images/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" type="image/png" href="/assets/images/favicon-16x16.png" sizes="16x16" />
+		<link rel="icon" type="image/png" href="/assets/images/favicon-128.png" sizes="128x128" />
+		<meta name="application-name" content="&nbsp;"/>
+		<meta name="msapplication-TileColor" content="#FFFFFF" />
+		<meta name="msapplication-TileImage" content="/assets/images/mstile-144x144.png" />
+		<meta name="msapplication-square70x70logo" content="/assets/images/mstile-70x70.png" />
+		<meta name="msapplication-square150x150logo" content="/assets/images/mstile-150x150.png" />
+		<meta name="msapplication-wide310x150logo" content="/assets/images/mstile-310x150.png" />
+		<meta name="msapplication-square310x310logo" content="/assets/images/mstile-310x310.png" />
+		
+		<!-- styles -->
+		<link href="/assets/css/main.css" rel="stylesheet" />
+		<link href="/assets/css/style.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div class="wrapper">
+			
+			<!-- header -->
+			<?php include('_header.php'); ?>
+			
+			
+			<!-- hero start -->
+			<section class="hero">
+				<div class="container">
+					<div class="row d-flex align-items-center">
+						<div class="col-lg-7">
+							<h1 class="display-4">Welcome to <span class="text-primary">WebTerms</span> Online.</h1>
+							<p class="mt-4">Welcome to WebTerms. Here you can generate your own Privacy Policy to comply with international laws on user data privacy (CCPA, CalOPPA, GDPR) and the requirements of leading Advertising and Affiliate Networks, such as Google AdSense or Commission Junction.<br><br>
+							On our site you will find not only a generator for quickly and easily generating a Privacy Policy for your sites, but also find many other resources for complying with regulations and learning more about Privacy Policies.</p>
+						</div>
+						<div class="col-lg-5 text-center">
+							<svg xmlns="http://www.w3.org/2000/svg" class="img-fluid" xmlns:xlink="http://www.w3.org/1999/xlink" width="333.7" height="320" viewBox="0 0 333.7 320">
+								<defs>
+								<clipPath id="clip-path">
+								<path id="Path_228" data-name="Path 228" d="M266.5,86.4H17.5l-.6,180.8h249Z"/>
+								</clipPath>
+								<clipPath id="clip-path-2">
+								<path id="Path_231" data-name="Path 231" d="M274.4,69.3H9.8A6.7,6.7,0,0,0,3.1,76L2.4,277.7a6.7,6.7,0,0,0,6.7,6.7H273.6a6.7,6.7,0,0,0,6.7-6.7L281,75.9A6.594,6.594,0,0,0,274.4,69.3Z"/>
+								</clipPath>
+								<linearGradient id="linear-gradient" x1="0.155" y1="0.053" x2="0.791" y2="0.921" gradientUnits="objectBoundingBox">
+									<stop offset="0" stop-color="#fff" stop-opacity="0"/>
+									<stop offset="1"/>
+								</linearGradient>
+								<linearGradient id="linear-gradient-2" x1="-0.4" y1="0.74" x2="0.768" y2="0.501" gradientUnits="objectBoundingBox">
+									<stop offset="0" stop-opacity="0"/>
+									<stop offset="1"/>
+								</linearGradient>
+								<linearGradient id="linear-gradient-3" x1="0.645" y1="0.979" x2="-0.049" y2="0.358" xlink:href="#linear-gradient-2"/>
+									</defs>
+									<g id="bg" transform="translate(0 -15.5)">
+										<path id="Path_211" data-name="Path 211" d="M292.3,328.7c0-3.8-65.4-6.8-146.1-6.8C65.4,321.8,0,324.9,0,328.7s65.4,6.8,146.1,6.8S292.3,332.4,292.3,328.7Z" fill="#e1e9f4" opacity="0.5"/>
+										<g id="Group_221" data-name="Group 221">
+											<g id="Group_207" data-name="Group 207">
+												<g id="Group_205" data-name="Group 205">
+													<path id="Path_212" data-name="Path 212" d="M333.7,195h0c-11.6,0-21-8.9-21-19.8V77.1c0-12.8-6.6-23.2-14.7-23.2h-2c-8.1,0-21.4,10.4-21.4,23.2h-64V159H169.8v36Z" fill="#a1b3d1"/>
+													<path id="Path_213" data-name="Path 213" d="M315.9,195h0c-10.3,0-18.7-8.9-18.7-19.8V77.1c0-12.8-5.9-23.2-13.1-23.2h-1.8c-7.2,0-19.1,10.4-19.1,23.2H206.1V159H169.8v36Z" opacity="0.15"/>
+													<path id="Path_214" data-name="Path 214" d="M299.6,54H164.9c-10.9,0-19.8,10.4-19.8,23.2v1.1h0V259c0,12.8-4.9,23.2-13.9,23.2H266.5c9,0,16.3-10.4,16.3-23.2V78.2h0V77.1C282.7,64.3,288.7,54,299.6,54Z" fill="#d0ddf5"/>
+													<path id="Path_215" data-name="Path 215" d="M283.6,54.4H148.9c-10.9,0-19.8,10.4-19.8,23.2v1.1h0V259.4c0,12.8-4.9,23.2-13.9,23.2H250.5c9,0,16.3-10.4,16.3-23.2V78.7h0V77.6C266.7,64.8,272.7,54.4,283.6,54.4Z" fill="#dedede" opacity="0.59"/>
+												</g>
+												<g id="Group_206" data-name="Group 206">
+													<path id="Path_216" data-name="Path 216" d="M258.5,259v-4.7h0V38.7c0-12.8-8.2-23.2-18.3-23.2h-2.5c-10.1,0-26.7,10.4-26.7,23.2H131.2v81.9H80.5V259c0,12.8,11.5,23.2,25.6,23.2H277.5C263.3,282.1,258.5,271.8,258.5,259Z" fill="#a1b3d1"/>
+													<path id="Path_217" data-name="Path 217" d="M237.9,259v-4.7h0V38.7c0-12.8-8.2-23.2-18.2-23.2h-2.5c-10.1,0-26.6,10.4-26.6,23.2H131.1v81.9H80.5V259c0,12.8,11.4,23.2,25.5,23.2H256.9C242.7,282.1,237.9,271.8,237.9,259Z" opacity="0.15"/>
+												</g>
+											</g>
+											<g id="Group_214" data-name="Group 214">
+												<g id="Group_209" data-name="Group 209">
+													<g id="Group_208" data-name="Group 208">
+														<path id="Path_218" data-name="Path 218" d="M189,278.6H123.3L118,323.4h76.3Z" fill="#8f8f8f"/>
+														<path id="Path_219" data-name="Path 219" d="M186.3,278.6H120.5l-5.3,44.8h76.4Z" fill="#d6d6d6"/>
+														<path id="Path_220" data-name="Path 220" d="M186.3,278.6H120.5l-2.3,23.1h70.4Z" fill="#c7c7c7"/>
+													</g>
+													<path id="Path_221" data-name="Path 221" d="M109.4,328.5h0c0-4.1,2.8-7.5,6.3-7.5h97.1c3.5,0,6.3,3.3,6.3,7.5H109.4Z" fill="#ccc"/>
+													<path id="Path_222" data-name="Path 222" d="M91.6,328.5h0c0-4.1,2.8-7.5,6.3-7.5H195c3.5,0,6.3,3.3,6.3,7.5H91.6Z" fill="#f2f2f2"/>
+												</g>
+												<g id="Group_213" data-name="Group 213">
+													<g id="Group_210" data-name="Group 210">
+														<path id="Path_223" data-name="Path 223" d="M283.7,69.8H13.5a6.874,6.874,0,0,0-6.9,6.7l-1,201.3a6.723,6.723,0,0,0,6.8,6.7H282.6a6.874,6.874,0,0,0,6.9-6.7l1-201.3A6.723,6.723,0,0,0,283.7,69.8Z" fill="#b2b4b7"/>
+													</g>
+													<path id="Path_224" data-name="Path 224" d="M274.4,69.3H9.8A6.7,6.7,0,0,0,3.1,76L2.4,277.7a6.7,6.7,0,0,0,6.7,6.7H273.6a6.7,6.7,0,0,0,6.7-6.7L281,75.9A6.594,6.594,0,0,0,274.4,69.3Z" fill="#d6d6d6"/>
+													<g id="Group_212" data-name="Group 212">
+														<path id="Path_225" data-name="Path 225" d="M266.5,86.4H17.5l-.6,180.8h249Z" fill="#333838"/>
+														<g id="Group_211" data-name="Group 211" opacity="0.06" clip-path="url(#clip-path)">
+															<path id="Path_226" data-name="Path 226" d="M81.8,332.1l-69-59.9L180.8,0l69,59.9Z" fill="#fff"/>
+															<path id="Path_227" data-name="Path 227" d="M108.1,355.7l-21-18.2L255,65.3l21,18.2Z" fill="#fff"/>
+														</g>
+													</g>
+													<path id="Path_229" data-name="Path 229" d="M144.3,77.5a2.4,2.4,0,0,1-2.2,2.6,2.459,2.459,0,0,1-2.2-2.6,2.4,2.4,0,0,1,2.2-2.6C143.3,74.8,144.3,76,144.3,77.5Z" fill="gray" opacity="0.46"/>
+												</g>
+											</g>
+											<g id="Group_216" data-name="Group 216">
+												<g id="Group_215" data-name="Group 215" clip-path="url(#clip-path-2)">
+													<path id="Path_230" data-name="Path 230" d="M239.6,255.3H70.8a5.378,5.378,0,0,1-5.4-5.4V75.7H245V249.9A5.378,5.378,0,0,1,239.6,255.3Z" fill="#242424" opacity="0.3"/>
+												</g>
+											</g>
+											<g id="Group_220" data-name="Group 220">
+												<g id="Group_219" data-name="Group 219">
+													<path id="Path_232" data-name="Path 232" d="M46.9,236.8H215.7a4.549,4.549,0,0,0,4.6-4.4h0V38.7c0-12.8,7.8-23.2,21.9-23.2H67.9c-14.1,0-25.6,10.4-25.6,23.2V232.3h0A4.567,4.567,0,0,0,46.9,236.8Z" fill="#e1e9f4"/>
+													<g id="Group_218" data-name="Group 218">
+														<g id="Group_217" data-name="Group 217">
+															<path id="Path_233" data-name="Path 233" d="M189,160.1H72.5a2.974,2.974,0,0,1-3-2.8h0a2.845,2.845,0,0,1,3-2.8H189a2.974,2.974,0,0,1,3,2.8h0A2.974,2.974,0,0,1,189,160.1Z" fill="#4b82e7"/>
+															<path id="Path_234" data-name="Path 234" d="M189,175.8H72.5a2.974,2.974,0,0,1-3-2.8h0a2.845,2.845,0,0,1,3-2.8H189a2.974,2.974,0,0,1,3,2.8h0A2.974,2.974,0,0,1,189,175.8Z" fill="#4b82e7"/>
+															<path id="Path_235" data-name="Path 235" d="M189,191.5H72.5a2.974,2.974,0,0,1-3-2.8h0a2.845,2.845,0,0,1,3-2.8H189a2.974,2.974,0,0,1,3,2.8h0A2.906,2.906,0,0,1,189,191.5Z" fill="#4b82e7"/>
+															<path id="Path_236" data-name="Path 236" d="M159,207.3H72.5a2.974,2.974,0,0,1-3-2.8h0a2.845,2.845,0,0,1,3-2.8H159a2.974,2.974,0,0,1,3,2.8h0A2.974,2.974,0,0,1,159,207.3Z" fill="#4b82e7"/>
+														</g>
+													</g>
+												</g>
+											</g>
+											<path id="Path_237" data-name="Path 237" d="M162,72.4v45.4a6.487,6.487,0,0,1-6.5,6.5H115a7.17,7.17,0,0,1-7.2-7.2,1.617,1.617,0,0,1,1.4-1.6,14.994,14.994,0,0,0,13.6-14.9,15.027,15.027,0,0,0-12.2-14.8h0c-.2,0-.4-.1-.7-.1h-.1c-.3-.1-.6-.1-.9-.2h0c-.3-.1-.6-.2-1-.3-3.2-1-6.1-3-6.1-5.5,0-3.2,3.2-5.9,7.1-5.9h0c4.8-.5,8.4-3.6,8.4-7.2,0-2.9-2.3-5.4-5.6-6.6a2.926,2.926,0,0,1-.8-5.1,6.979,6.979,0,0,1,4-1.2h27.6a1.188,1.188,0,0,1,1,1.2v11a1.445,1.445,0,0,0,.2.7c1.3,1.8,2.2,2.3,4.1,4a1.222,1.222,0,0,0,.9.3h11.9C161.5,71.2,162,71.7,162,72.4Z" fill="#0b4ff3"/>
+											<path id="Path_238" data-name="Path 238" d="M162,72.4v45.4a6.487,6.487,0,0,1-6.5,6.5H115a7.17,7.17,0,0,1-7.2-7.2,1.617,1.617,0,0,1,1.4-1.6,14.994,14.994,0,0,0,13.6-14.9,15.027,15.027,0,0,0-12.2-14.8h0c-.2,0-.4-.1-.7-.1h-.1c-.3-.1-.6-.1-.9-.2h0c-.3-.1-.6-.2-1-.3-3.2-1-6.1-3-6.1-5.5,0-3.2,3.2-5.9,7.1-5.9h0c4.8-.5,8.4-3.6,8.4-7.2,0-2.9-2.3-5.4-5.6-6.6a2.926,2.926,0,0,1-.8-5.1,6.979,6.979,0,0,1,4-1.2h27.6a1.188,1.188,0,0,1,1,1.2v11a1.445,1.445,0,0,0,.2.7c1.3,1.8,2.2,2.3,4.1,4a1.222,1.222,0,0,0,.9.3h11.9C161.5,71.2,162,71.7,162,72.4Z" opacity="0.17" fill="url(#linear-gradient)"/>
+											<path id="Path_239" data-name="Path 239" d="M162,72.4v45.4a6.487,6.487,0,0,1-6.5,6.5H115a7.17,7.17,0,0,1-7.2-7.2,1.617,1.617,0,0,1,1.4-1.6,14.994,14.994,0,0,0,13.6-14.9,15.317,15.317,0,0,0-3.6-9.8l.2-.4,10.7-16.9,12.5-19.8a1.188,1.188,0,0,1,1,1.2v11a1.445,1.445,0,0,0,.2.7c1.3,1.8,2.2,2.3,4.1,4a1.222,1.222,0,0,0,.9.3h11.9C161.5,71.2,162,71.7,162,72.4Z" opacity="0.3" fill="url(#linear-gradient-2)"/>
+											<path id="Path_240" data-name="Path 240" d="M162,71.6v.5H148.3a5.7,5.7,0,0,1-5.7-5.7V53.8h.5Z" fill="#5bdcff"/>
+											<path id="Path_241" data-name="Path 241" d="M162,71.6v.5H148.3a5.7,5.7,0,0,1-5.7-5.7V53.8h.5Z" opacity="0.36" fill="url(#linear-gradient-3)"/>
+											<circle id="Ellipse_40" data-name="Ellipse 40" cx="2.8" cy="2.8" r="2.8" transform="translate(102.9 63.3)" fill="#0b4ff3"/>
+											<path id="Path_242" data-name="Path 242" d="M110.6,86Z" fill="#0b4ff3"/>
+											<circle id="Ellipse_41" data-name="Ellipse 41" cx="6.2" cy="6.2" r="6.2" transform="translate(104 94.3)" fill="#0b4ff3"/>
+											<circle id="Ellipse_42" data-name="Ellipse 42" cx="5" cy="5" r="5" transform="translate(87.5 86.9)" fill="#0b4ff3"/>
+											<circle id="Ellipse_43" data-name="Ellipse 43" cx="2.8" cy="2.8" r="2.8" transform="translate(94.4 115.1)" fill="#0b4ff3"/>
+										</g>
+									</g>
+								</svg>
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- hero end -->
+				<!-- generators start -->
+				<section class="sect-generators">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4 mt-5 mt-lg-0">
+								<svg id="icon-1" class="icon-pp-generator" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60">
+									<rect id="Rectangle_46" data-name="Rectangle 46" width="60" height="60" fill="#5bdcff" opacity="0"/>
+									<g id="Group_131" data-name="Group 131" transform="translate(8.4 3.4)">
+										<path id="Path_140" data-name="Path 140" d="M51.3,8.2H49.2c-.2,0-14.2,0-19.3-4.8h-.1C24.7,8.2,10.6,8.2,10.5,8.2H8.4V34.9h0L29.8,56.3h0L51.2,34.9h0V8.2ZM12.7,33.1V12.4c3.8-.2,11.8-.9,17.2-3.9,5.4,2.9,13.4,3.7,17.2,3.9V33.1L29.8,50.3Z" transform="translate(-8.4 -3.4)" fill="#b6c7ee"/>
+										<path id="Path_141" data-name="Path 141" d="M29.8,7.2c-6.1,4.1-18.4,4.1-18.4,4.1V33.8L29.8,52.2,48.2,33.8V11.3S36,11.3,29.8,7.2Z" transform="translate(-8.4 -3.4)" fill="#0b4ff3"/>
+										<path id="Path_142" data-name="Path 142" d="M27.6,33.3a1.905,1.905,0,0,1-1-.5l-5.8-4.5,3.1-3.9L28,27.5l7.5-7.7L39,23.4l-9.1,9.2a2.267,2.267,0,0,1-2.3.7Z" transform="translate(-8.4 -3.4)" fill="#fff"/>
+									</g>
+								</svg>
+								<h4 class="mt-3">Privacy Policy Generator</h4>
+								<p class="mt-4">Use this simple web form to generate a Privacy Policy document for your website.</p>
+								<a href="/privacy-policy-generator/" class="btn btn-outline-primary mt-4">Generate Privacy Policy</a>
+							</div>
+							<div class="col-lg-4 mt-5 mt-lg-0">
+								<svg id="icon-2" class="icon-tc-generator" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60">
+									<rect id="Rectangle_43" data-name="Rectangle 43" width="60" height="60" fill="#5bdcff" opacity="0"/>
+									<g id="Group_127" data-name="Group 127" transform="translate(12.5 6)">
+										<path id="Path_133" data-name="Path 133" d="M42.8,27.7H38.5V19a8.7,8.7,0,1,0-17.4,0v8.7H16.8V19a13,13,0,1,1,26,0Z" transform="translate(-12.5 -6)" fill="#b6c7ee"/>
+										<rect id="Rectangle_44" data-name="Rectangle 44" width="34.7" height="28.2" transform="translate(0 19.5)" fill="#0b4ff3"/>
+										<path id="Path_134" data-name="Path 134" d="M34.2,36.3a4.3,4.3,0,0,0-8.6,0,4.406,4.406,0,0,0,2.2,3.8v7.1H32V40.1a4.288,4.288,0,0,0,2.2-3.8Z" transform="translate(-12.5 -6)" fill="#fff"/>
+									</g>
+								</svg>
+								<h4 class="mt-3">Terms & ConditionsGenerator</h4>
+								<p class="mt-4">Use this simple web form to generate a Terms & Conditions document for your website.</p>
+								<a href="/terms-conditions-generator/" class="btn btn-outline-primary mt-4">Generate Terms & Conditions</a>
+							</div>
+							<div class="col-lg-4 mt-5 mt-lg-0">
+								<svg id="icon-3" class="icon-d-generator" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60">
+									<rect id="Rectangle_45" data-name="Rectangle 45" width="60" height="60" fill="#5bdcff" opacity="0"/>
+									<g id="Group_129" data-name="Group 129" transform="translate(6.6 6.6)">
+										<path id="Path_135" data-name="Path 135" d="M26,39.5h7.8a1.9,1.9,0,0,1,1.9,1.9v9.7A1.9,1.9,0,0,1,33.8,53H26a1.9,1.9,0,0,1-1.9-1.9V41.4A1.837,1.837,0,0,1,26,39.5Z" transform="translate(-6.6 -6.6)" fill="#0b4ff3"/>
+										<path id="Path_136" data-name="Path 136" d="M19.9,49.2H39.5v3.9H19.9Z" transform="translate(-6.6 -6.6)" fill="#b6c7ee"/>
+										<path id="Path_137" data-name="Path 137" d="M8.5,6.6H51.1A1.9,1.9,0,0,1,53,8.5v31a1.9,1.9,0,0,1-1.9,1.9H8.5a1.9,1.9,0,0,1-1.9-1.9V8.5A1.967,1.967,0,0,1,8.5,6.6Z" transform="translate(-6.6 -6.6)" fill="#b6c7ee"/>
+										<path id="Path_138" data-name="Path 138" d="M53.1,37.6V6.6H6.6v31Z" transform="translate(-6.6 -6.6)" fill="#0b4ff3"/>
+									</g>
+									<path id="Path_143" data-name="Path 143" d="M27.6,33.3a1.905,1.905,0,0,1-1-.5l-5.8-4.5,3.1-3.9L28,27.5l7.5-7.7L39,23.4l-9.1,9.2a2.267,2.267,0,0,1-2.3.7Z" transform="translate(0 -4.5)" fill="#fff"/>
+								</svg>
+								<h4 class="mt-3">Disclaimer Generator</h4>
+								<p class="mt-4">Use this simple web form to generate a Disclaimer document for your website.</p>
+								<a href="/disclaimerpolicy-generator/" class="btn btn-outline-primary mt-4">Generate Disclaimer</a>
+							</div>
+							
+						</div>
+					</div>
+				</section>
+				<!-- generators end -->
+				
+				<!-- blog start -->
+				<section class="sect-blog">
+					<div class="container">
+						<div class="row">
+							<div class="col">
+								<h2 class="text-center">Blog</h2>
+							</div>
+						</div>
+						<div class="row mt-5">
+							<div class="col-lg-4 mt-5 mt-lg-0">
+								<div class="card h-100">
+									<img src="https://via.placeholder.com/400x300/0B4FF3/cfddff?Text=image-here" alt="" class="card-img-top">
+									<div class="card-body text-center pb-5">
+										<h3>What Is A Privacy Policy?</h3>
+										<p class="mt-4">A Privacy Policy is a legal document that discloses some or all of the ways a website gathers, uses, shares, discloses and/or manages a website visitor’s data.</p>
+										<a href="#" class="btn btn-link position-absolute fixed-bottom">Read More</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mt-5 mt-lg-0">
+								<div class="card h-100">
+									<img src="https://via.placeholder.com/400x300/0B4FF3/cfddff?Text=image-here" alt="" class="card-img-top">
+									<div class="card-body text-center pb-5">
+										<h3>What Is A Privacy Policy?</h3>
+										<p class="mt-4">A Privacy Policy is a legal document that discloses some or all of the ways a website gathers, uses, shares, discloses and/or manages a website visitor’s data. uses, shares, discloses and/or manages a website visitor’s data. e visitor’s data e visitor’s data e visitor’s data</p>
+										<a href="#" class="btn btn-link position-absolute fixed-bottom">Read More</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 mt-5 mt-lg-0">
+								<div class="card h-100">
+									<img src="https://via.placeholder.com/400x300/0B4FF3/cfddff?Text=image-here" alt="" class="card-img-top">
+									<div class="card-body text-center pb-5">
+										<h3>What Is A Privacy Policy?</h3>
+										<p class="mt-4">A Privacy Policy is a legal document that discloses some or all of the ways a website gathers, uses, shares, discloses and/or manages a website visitor’s data.</p>
+										<a href="#" class="btn btn-link position-absolute fixed-bottom">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- blog end -->
+				
+				<!-- footer -->
+				<?php include('_footer.php'); ?>
+			</div>
+			
+			
+			
+		</body>
+	</html>
